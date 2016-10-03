97db8f0eb26f785305ca8c90a2045a989936fdfd
George Kaniouris <george.kaniouris@cultbeauty.co.uk>
2016-10-03 12:24:53 +0100

    Styled nav bar

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
+import Navigation from "./Navigation";
+
 var Main = (props) => {
     return <div>
+      <Navigation />
       <div>
         <div>
           {props.children}
---
+++ app/components/Navigation.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link, IndexLink} from 'react-router';
+
+var Navigation = () => {
+  return <div className="top-bar">
+    <div className="top-bar-left">
+      <ul className="menu">
+        <li className="menu-text">React Time App</li>
+        <li><IndexLink to="/" activeClassName="active-link">Timer</IndexLink></li>
+        <li><Link to="/" activeClassName="active-link">Countdown</Link></li>
+      </ul>
+    </div>
+    <div className="top-bar-right">
+      <ul className="menu">
+        <li className="menu-text">Created by <a href="https://github.com/gkaniouris" target="_blank">George</a></li>
+      </ul>
+    </div>
+  </div>
+};
+
+export default Navigation;

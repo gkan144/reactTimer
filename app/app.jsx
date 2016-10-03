db1f5b80d8beaa1051bf59c51cf1f4ffc19f1af2
George Kaniouris <george.kaniouris@cultbeauty.co.uk>
2016-10-03 13:09:12 +0100

    Basic navigation done

--- app/app.jsx
+++ app/app.jsx
@@ -7,8 +7,12 @@ $(document).foundation();
 require('style!css!sass!./styles/app.scss');
 
 import Main from './components/Main';
+import Timer from './components/Timer';
+import Countdown from './components/Countdown';
 
 ReactDOM.render(<Router history={hashHistory}>
   <Route path="/" component={Main}>
+    <Route path="countdown" component={Countdown}/>
+    <IndexRoute component={Timer}/>
   </Route>
 </Router>, document.getElementById('app'));
---
+++ app/components/Countdown.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Countdown extends React.Component {
+  render() {
+    return <div>
+      Countdown
+    </div>
+  }
+}
+
+export default Countdown;
